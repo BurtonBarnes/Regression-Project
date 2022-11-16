60219cbcf72989cb64b24820f0e131455af26f83
Burton Barnes <barnesburton@gmail.com>
2022-11-16 16:49:38 -0600

    ongoing

--- wrangle.py
+++ wrangle.py
@@ -5,6 +5,11 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 from scipy import stats
+import sklearn
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import explained_variance_score
+from sklearn.linear_model import LinearRegression, LassoLars, TweedieRegressor
+from sklearn.preprocessing import PolynomialFeatures
 
 # acquire
 from env import get_db_url
@@ -136,3 +141,225 @@ def get_chi_bedrooms(train):
     
     
     
+# prep data for modeling
+def model_prep(train, validate, test):
+    '''Prepare train, validate, and test data for modeling'''
+    
+    # drop unused columns
+    keep_cols = ['property_value',
+                 'fips',
+                 'yearbuilt',
+                 'bathrooms',
+                 'bedrooms'
+                ]
+    
+    train = train[keep_cols]
+    validate = validate[keep_cols]
+    test = test[keep_cols]
+    
+    # Split data into predicting variables (X) and target variable (y) and reset the index for each dataframe
+    train_X = train.drop(columns='property_value').reset_index(drop=True)
+    train_y = train[['property_value']].reset_index(drop=True)
+    
+    validate_X = validate.drop(columns='property_value').reset_index(drop=True)
+    validate_y = validate[['property_value']].reset_index(drop=True)
+    
+    test_X = test.drop(columns='property_value').reset_index(drop=True)
+    test_y = test[['property_value']].reset_index(drop=True)
+    
+    return train_X, validate_X, test_X, train_y, validate_y, test_y
+
+
+
+
+def scale_data(train_X, validate_X, test_X):
+    # Scale the data
+    scaler = sklearn.preprocessing.MinMaxScaler()
+
+    # Fit the scaler
+    scaler.fit(train_X)
+
+    # Use the scaler to transform train, validate, test
+    X_train_scaled = scaler.transform(train_X)
+    X_validate_scaled = scaler.transform(validate_X)
+    X_test_scaled = scaler.transform(test_X)
+
+
+    # Turn everything into a dataframe
+    X_train_scaled = pd.DataFrame(X_train_scaled, columns=train_X.columns)
+    X_validate_scaled = pd.DataFrame(X_validate_scaled, columns=train_X.columns)
+    X_test_scaled = pd.DataFrame(X_test_scaled, columns=train_X.columns)
+    return X_train_scaled, X_validate_scaled, X_test_scaled
+
+
+
+
+def get_mean(train_y, validate_y):
+    # We need y_train and y_validate to be dataframes to append the new columns with predicted values.
+    y_train = pd.DataFrame(train_y)
+    y_validate = pd.DataFrame(validate_y)
+    
+    # Predict property_value_pred_mean
+    property_value_pred_mean = train_y.property_value.mean()
+    train_y['property_value_pred_mean'] = property_value_pred_mean
+    validate_y['property_value_pred_mean'] = property_value_pred_mean
+    
+    # compute property_value_pred_median
+    property_value_pred_median = train_y.property_value.median()
+    train_y['property_value_pred_median'] = property_value_pred_median
+    validate_y['property_value_pred_median'] = property_value_pred_median
+    
+    # RMSE of property_value_pred_mean
+    rmse_train = mean_squared_error(y_train.property_value,
+                                y_train.property_value_pred_mean) ** .5
+    rmse_validate = mean_squared_error(y_validate.property_value, y_validate.property_value_pred_mean) ** (1/2)
+    
+    print("RMSE using Mean\nTrain/In-Sample: ", round(rmse_train, 2), 
+      "\nValidate/Out-of-Sample: ", round(rmse_validate, 2))
+    
+    # RMSE of property_value_pred_median
+    rmse_train = mean_squared_error(y_train.property_value, y_train.property_value_pred_median) ** .5
+    rmse_validate = mean_squared_error(y_validate.property_value, y_validate.property_value_pred_median) ** .5
+    print("RMSE using Median\nTrain/In-Sample: ", round(rmse_train, 2), 
+      "\nValidate/Out-of-Sample: ", round(rmse_validate, 2))
+    
+    
+    
+    
+def make_metric_df(y, y_pred, model_name, metric_df):
+    if metric_df.size ==0:
+        metric_df = pd.DataFrame(data=[
+            {
+                'model': model_name, 
+                'RMSE_validate': mean_squared_error(
+                    y,
+                    y_pred) ** .5,
+                'r^2_validate': explained_variance_score(
+                    y,
+                    y_pred)
+            }])
+        return metric_df
+    else:
+        return metric_df.append(
+            {
+                'model': model_name, 
+                'RMSE_validate': mean_squared_error(
+                    y,
+                    y_pred) ** .5,
+                'r^2_validate': explained_variance_score(
+                    y,
+                    y_pred)
+            }, ignore_index=True)
+    
+    
+    
+    
+def linear_regression(train_X, train_y, validate_X, validate_y):
+    lm = LinearRegression(normalize=True)
+    lm.fit(train_X, train_y.property_value)
+    train_y['property_value_pred_lm'] = lm.predict(train_X)
+    
+    # evaluate: rmse
+    rmse_train = mean_squared_error(train_y.property_value, train_y.property_value_pred_lm) ** (1/2)
+
+    # predict validate
+    validate_y['property_value_pred_lm'] = lm.predict(validate_X)
+
+    # evaluate: rmse
+    rmse_validate = mean_squared_error(validate_y.property_value, validate_y.property_value_pred_lm) ** (1/2)
+
+    print("RMSE for OLS using LinearRegression\nTraining/In-Sample: ", rmse_train, 
+      "\nValidation/Out-of-Sample: ", rmse_validate)
+    return rmse_train, rmse_validate
+
+
+
+
+def lassolars(train_X, train_y, validate_X, validate_y):
+    # create the model object
+    lars = LassoLars(alpha=1)
+
+    # fit the model to our training data. We must specify the column in y_train, 
+    # since we have converted it to a dataframe from a series!
+    lars.fit(train_X, train_y.property_value)
+
+    # predict train
+    train_y['property_value_pred_lars'] = lars.predict(train_X)
+
+    # evaluate: rmse
+    rmse_train = mean_squared_error(train_y.property_value, train_y.property_value_pred_lars) ** (1/2)
+
+    # predict validate
+    validate_y['property_value_pred_lars'] = lars.predict(validate_X)
+
+    # evaluate: rmse
+    rmse_validate = mean_squared_error(validate_y.property_value, validate_y.property_value_pred_lars) ** (1/2)
+
+    print("RMSE for Lasso + Lars\nTraining/In-Sample: ", rmse_train, 
+      "\nValidation/Out-of-Sample: ", rmse_validate)
+    return rmse_train, rmse_validate
+
+
+
+
+def tweedie(train_X, train_y, validate_X, validate_y):
+    # create the model object
+    glm = TweedieRegressor(power=1, alpha=0)
+
+
+    # fit the model to our training data. We must specify the column in y_train, 
+    # since we have converted it to a dataframe from a series! 
+    glm.fit(train_X, train_y.property_value)
+
+    # predict train
+    train_y['property_value_pred_glm'] = glm.predict(train_X)
+
+    # evaluate: rmse
+    rmse_train = mean_squared_error(train_y.property_value, train_y.property_value_pred_glm) ** (1/2)
+
+    # predict validate
+    validate_y['property_value_pred_glm'] = glm.predict(validate_X)
+
+    # evaluate: rmse
+    rmse_validate = mean_squared_error(validate_y.property_value, validate_y.property_value_pred_glm) ** (1/2)
+
+    print("RMSE for GLM using Tweedie, power=1 & alpha=0\nTraining/In-Sample: ", rmse_train, 
+      "\nValidation/Out-of-Sample: ", rmse_validate)
+    return rmse_train, rmse_validate
+
+
+
+
+def linear(train_X, train_y, validate_X, validate_y, test_X):
+    # make the polynomial features to get a new set of features
+    pf = PolynomialFeatures(degree=2)
+
+    # fit and transform X_train_scaled
+    X_train_degree2 = pf.fit_transform(train_X)
+
+    # transform X_validate_scaled & X_test_scaled
+    X_validate_degree2 = pf.transform(validate_X)
+    X_test_degree2 =  pf.transform(test_X)
+    
+    # create the model object
+    lm2 = LinearRegression(normalize=True)
+
+    # fit the model to our training data. We must specify the column in y_train, 
+    # since we have converted it to a dataframe from a series! 
+    lm2.fit(X_train_degree2, train_y.property_value)
+
+    # predict train
+    train_y['property_value_pred_lm2'] = lm2.predict(X_train_degree2)
+
+    # evaluate: rmse
+    rmse_train = mean_squared_error(train_y.property_value, train_y.property_value_pred_lm2) ** (1/2)
+
+    # predict validate
+    validate_y['property_value_pred_lm2'] = lm2.predict(X_validate_degree2)
+
+    # evaluate: rmse
+    rmse_validate = mean_squared_error(validate_y.property_value, validate_y.property_value_pred_lm2) ** 0.5
+
+    print("RMSE for Polynomial Model, degrees=2\nTraining/In-Sample: ", rmse_train, 
+      "\nValidation/Out-of-Sample: ", rmse_validate)
+    return rmse_train, rmse_validate
